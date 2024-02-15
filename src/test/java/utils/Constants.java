9dec2e2c648ba5c3548191a9e6fdde1be6b3b0b5
DamlaDay <damladayal@gmail.com>
2024-02-15 16:57:30 -0500

    the first commit

---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,14 @@
+package utils;
+
+
+import org.checkerframework.checker.units.qual.C;
+
+public class Constants {
+    public static final String CONFIG_FILE_PATH= System.getProperty("user.dir")+"/src/test/resources/config/config.properties";
+    public  static final int EXPLICIT_WAIT=20;
+    public static final int IMPLICIT_WAIT=10;
+    public static final String EXCEL_FILE_PATH=System.getProperty("user.dir")+"/src/test/resources/testdata/Batch18cucumberexcel.xlsx";
+    public static final String SCREENSHOT_FILEPATH = System.getProperty("user.dir")+"/screenshots/";
+}
+
+
